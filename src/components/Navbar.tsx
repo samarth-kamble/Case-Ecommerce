28d22869b956e27f517a74706caf2bd21ae344ac
Samarth Kamble <kamble.samarth91@gmail.com>
2024-06-21 22:52:58 +0530

    Implement the footer and auth with kinde

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,23 @@
-import React from "react";
-import MaxWidthWrapper from "./MaxWidthWrapper";
 import Link from "next/link";
-import { ArrowRight } from "lucide-react";
+import MaxWidthWrapper from "./MaxWidthWrapper";
 import { buttonVariants } from "./ui/button";
+import { ArrowRight } from "lucide-react";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
 const Navbar = async () => {
-  const user = undefined;
-  const isAdmin = false;
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  const isAdmin = user?.email === process.env.ADMIN_EMAIL;
+
   return (
     <nav className="sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
       <MaxWidthWrapper>
         <div className="flex h-14 items-center justify-between border-b border-zinc-200">
-          <Link className="flex z-40    font-semibold" href="/">
+          <Link href="/" className="flex z-40 font-semibold">
             Case<span className="text-green-600">Cobra</span>
           </Link>
+
           <div className="h-full flex items-center space-x-4">
             {user ? (
               <>
